aba003bfd3c48e4979d5b814c3a424907d4e1c96
Chao Wang <cwang8023@gmail.com>
2014-01-13 09:07:46 +0800

    make bluetooth work

--- www/js/bluetooth.js
+++ www/js/bluetooth.js
@@ -1,6 +1,6 @@
 bluetooth = (function(){
     function bindEvents() {
-        $("#connectBtn").on("vclick", function () {
+        $("#connectDevice").on("click", function () {
             flash.info("start connect in connect");
             connect();
         });
@@ -28,6 +28,7 @@ bluetooth = (function(){
         flash.info("start connect");
         var device = $('#deviceList input[data-cacheval="true"]').val();
         console.log("Requesting connection to " + device);
+        flash.info(device +":"+ $('#deviceList input[data-cacheval="true"]').attr("id"));
         bluetoothSerial.connect(device, onConnect, onDisconnect);
     }
 
@@ -39,11 +40,13 @@ bluetooth = (function(){
     }
 
     function onConnect() {
+        flash.info("Connected");
+
         $("#connectDevice").text("Connected");
     }
 
-    function onDisconnect() {
-        flash.info("bt connect failed");
+    function onDisconnect(reason) {
+        flash.info("Connect failed: " + reason);
     }
 
     // Bind Event Listeners
@@ -59,7 +62,7 @@ bluetooth = (function(){
 
         devices.forEach(function (device) {
             option = $('<input>',{name:'device', type:'radio', id:device.name})
-            flash.info("Found device: "+device.name);
+            flash.info(device.name + " addr: " + device.address);
             if (device.hasOwnProperty("uuid")) {
                 option.val(device.uuid)
             } else if (device.hasOwnProperty("address")) {
@@ -68,7 +71,7 @@ bluetooth = (function(){
                 option.val("ERROR " + JSON.stringify(device));
             }
             option.html(device.name);
-            $("#deviceList").append(option).append($('<label>',{for:device.name ,text:device.name}));
+            $("#deviceList").append(option).append($('<label>',{for:device.name ,text:device.name+"123" }));
             $("#deviceList").append(option);
         });
 
